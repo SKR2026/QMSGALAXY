6d64ac0bff5ef18a7aac03a417653ea21c2c2e35
SKR2026 <sushil@polyplasticsindia.com>
2026-07-11 12:14:11 +0530

    Add files via upload

---
+++ pp-cs/firebase-config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDeXLjY5K9o-DflzkUrfJ1uw2dMpq0lyMY",
+  authDomain: "skr-legal.firebaseapp.com",
+  databaseURL: "https://skr-legal-default-rtdb.firebaseio.com",
+  projectId: "skr-legal",
+  storageBucket: "skr-legal.firebasestorage.app",
+  messagingSenderId: "906193634469",
+  appId: "1:906193634469:web:c0f9257a6546b198e7f94c"
+};
+
+firebase.initializeApp(firebaseConfig);
